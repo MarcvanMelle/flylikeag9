cc167b47ed7c4fc4632eced3a97b155c748ae419
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-03 16:45:27 -0400

    Appease the Hound - Also removed useless test files.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,11 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifier  config.assets.compile = false
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
   config.log_level = :debug
   config.log_tags = [ :request_id ]
   config.action_mailer.perform_caching = false
@@ -14,7 +15,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
