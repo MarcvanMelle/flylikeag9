0727c97e317934649aeb3e2767d0e0b757c0f414
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-03 17:31:44 -0400

    Disabled Unncessary Spacing in rubocop, due to schema building with formatting space.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-
   root "words#index"
   devise_for :users, controllers: { registrations: 'registrations' }
-
   resources :reviews
   resources :words
   resources :users
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
