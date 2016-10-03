49ad3377cf1f3e6a068b3a557a6c28a7d2dbef54
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-03 17:08:14 -0400

    New rubocop config

--- db/migrate/20161003181354_add_devise_to_users.rb
+++ db/migrate/20161003181354_add_devise_to_users.rb
@@ -3,12 +3,9 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
     change_table :users do |t|
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-
       t.datetime :remember_created_at
-
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
