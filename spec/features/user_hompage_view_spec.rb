6efce45e7ab0a67aa11fa319bd763c7b2908204c
“keyanestgit config --global user.email “keyan.est@gmail.com <YOUR@EMAIL.com>
2016-10-04 12:03:22 -0400

    resolve remaining test issues

--- spec/features/user_hompage_view_spec.rb
+++ spec/features/user_hompage_view_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 require 'factory_girl'
-require_relative '../support/factories'
+require_relative '../support/factory'
 
 feature "user visits homepage" do
-  let!(:wordlist) { FactoryGirl.create_list(:word, 15) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:wordlist) { FactoryGirl.create_list(:word, 15, user: user) }
   context "and as a user" do
     scenario "I can see a list of the 10 most recently made words" do
       visit '/'
