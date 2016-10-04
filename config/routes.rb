f807061471e37d16f786ae446d5edf5c0bd5b61b
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-04 16:42:58 -0400

    Wrote tests & code for user login, user signup. Created partial for form submission. Overrode devise session controller for custom behavior (redirect_to signup when user DNE).

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "words#index"
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   resources :reviews
   resources :words, only: [:index]
   resources :users
