661f5218b21bcf5088bf80a8bb4dfbf2b9f72334
“keyanestgit config --global user.email “keyan.est@gmail.com <YOUR@EMAIL.com>
2016-10-04 13:25:31 -0400

    Fixed word instant variable on index by using .limit()

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,5 @@
 class WordsController < ApplicationController
   def index
-    all_words = Word.all.order!(created_at: :desc)
-    @words = all_words[0..9]
+    @words = Word.all.order!(created_at: :desc).limit(10)
   end
 end
