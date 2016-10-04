a285b1f899858faba6925130bd6073dcb9c5a66f
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-04 17:05:04 -0400

    Appease the Hound 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < Devise::SessionsController
-
   def index
     super
   end
@@ -10,7 +9,7 @@ class SessionsController < Devise::SessionsController
 
   def new
     user = User.find_by(email: params[:user][:email]) if params[:user]
-    
+
     if params[:user] && !user
       flash[:errors] = "You must create an account to sign in."
       redirect_to new_user_registration_path
