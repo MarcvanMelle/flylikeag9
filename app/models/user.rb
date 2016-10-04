fc2795c010189465a1862964033c36f73d4745c9
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-04 11:04:48 -0400

    Added tests and code for User Sign-Up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_many :words
   has_many :reviews
+
+  validates_uniqueness_of :username
+  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
