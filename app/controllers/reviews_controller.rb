3766e96d25680fb4a3a3227d977600b58dbf5c43
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 16:20:00 -0400

    got rid of unused variable

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,7 @@ class ReviewsController < ApplicationController
     review_text = params[:review][:review]
     word = params[:word_id]
     user = current_user
-    review = Review.create(rating: rating, review: review_text, word_id: word, user: user)
+    Review.create(rating: rating, review: review_text, word_id: word, user: user)
     redirect_to word_path(params[:word_id])
   end
 end
