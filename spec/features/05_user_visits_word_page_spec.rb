42856d6989a666e7ebcc85a3a1d4d4aa7b8a2eda
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 10:48:30 -0400

    Added link to word page from home page words

--- spec/features/05_user_visits_word_page_spec.rb
+++ spec/features/05_user_visits_word_page_spec.rb
@@ -4,9 +4,16 @@ feature "User visits word page" do
   let!(:user) { FactoryGirl.create(:user)}
   let!(:word) { FactoryGirl.create(:word, user: user) }
   context "As a user" do
-    scenario "I can click on a word and be taken to the word's page" do
+    scenario "I can click on a word on the home page and be taken to the word's page" do
+      visit root_path
+      click_link word.word
+
+      expect(page).to have_content word.word
+      expect(page).to have_content word.definition
+    end
+
+    scenario "I can click on a word on the index page and be taken to the word's page" do
       visit words_path
-      # save_and_open_page
       click_link word.word
 
       expect(page).to have_content word.word
