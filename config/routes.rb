9d3aa19f62558b6d09142827a1a5f0c77c4240ef
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 14:16:39 -0400

    Wrote tests & code for word update page. Checked for author and authentication status.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "words#home"
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   resources :reviews
-  resources :words, only: [:index, :home, :show, :edit]
+  resources :words, only: [:index, :home, :show, :edit, :update]
   resources :users
 end
