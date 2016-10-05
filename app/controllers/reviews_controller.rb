a6efe917e519e88f798bd724341ded15fc916c79
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 16:02:27 -0400

    Added test and functionality for signed in user posting review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,7 @@
 class ReviewsController < ApplicationController
+  def create
+    review = Review.new(rating: params[:review][:rating], review: params[:review][:review], word_id: params[:word_id], user: current_user)
+    review.save
+    redirect_to word_path(params[:word_id])
+  end
 end
