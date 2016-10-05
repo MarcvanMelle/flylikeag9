2f05d938ede24cccf854829829451afdd8422829
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 10:43:02 -0400

    Passed test for adding word show page

--- spec/features/05_user_visits_word_page_spec.rb
+++ spec/features/05_user_visits_word_page_spec.rb
@@ -6,6 +6,7 @@ feature "User visits word page" do
   context "As a user" do
     scenario "I can click on a word and be taken to the word's page" do
       visit words_path
+      # save_and_open_page
       click_link word.word
 
       expect(page).to have_content word.word
