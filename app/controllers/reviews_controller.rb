b0eea2d6eab94b5b281478480e3734e01a1f6cbf
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 16:14:05 -0400

    Hopefully the hound is done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,7 @@ class ReviewsController < ApplicationController
     review = params[:review][:review]
     word = params[:word_id]
     user = current_user
-    review = Review.new(rating: rating, review: review, word_id: word, user: user)
-    review.save
+    review = Review.create(rating: rating, review: review, word_id: word, user: user)
     redirect_to word_path(params[:word_id])
   end
 end
