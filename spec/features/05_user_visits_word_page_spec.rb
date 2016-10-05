f58323ee56c547b104c1d808c0da58a68106930b
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 10:52:21 -0400

    Appease the hound

--- spec/features/05_user_visits_word_page_spec.rb
+++ spec/features/05_user_visits_word_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "User visits word page" do
-  let!(:user) { FactoryGirl.create(:user)}
+  let!(:user) { FactoryGirl.create(:user) }
   let!(:word) { FactoryGirl.create(:word, user: user) }
   context "As a user" do
     scenario "I can click on a word on the home page and be taken to the word's page" do
