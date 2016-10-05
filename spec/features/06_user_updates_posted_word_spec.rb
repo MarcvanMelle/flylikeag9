46cead8053beb1396047824801a0cf1b299a9ab8
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 12:06:52 -0400

    Appeae the Hound

--- spec/features/06_user_updates_posted_word_spec.rb
+++ spec/features/06_user_updates_posted_word_spec.rb
@@ -6,7 +6,6 @@ feature "User updates a word they posted" do
   let!(:word2) { FactoryGirl.create(:word, user: user) }
   before { visit word_path(word1) }
 
-
   context "As a user" do
     scenario "I can click a link to go to my word's update page" do
       fill_in("Login-Email", with: user.email)
@@ -16,7 +15,6 @@ feature "User updates a word they posted" do
       visit word_path(word1)
       click_link "Update Content"
 
-      save_and_open_page
       expect(page).to have_content word1.word
       expect(page).to have_field "Definition"
       expect(page).to have_button "Update Word"
