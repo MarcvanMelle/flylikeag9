56bcdfab13bce1d6c93e2c5bb70e7d739f25fa93
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 16:15:22 -0400

    Another Hound issue

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,10 @@
 class ReviewsController < ApplicationController
   def create
     rating = params[:review][:rating]
-    review = params[:review][:review]
+    review_text = params[:review][:review]
     word = params[:word_id]
     user = current_user
-    review = Review.create(rating: rating, review: review, word_id: word, user: user)
+    review = Review.create(rating: rating, review: review_text, word_id: word, user: user)
     redirect_to word_path(params[:word_id])
   end
 end
