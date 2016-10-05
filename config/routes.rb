7c99ad2fef570ef688d268f252b4764a4a8d6198
michaelgt04 <michaelgt04@gmail.com>
2016-10-05 10:43:02 -0400

    Passed test for adding word show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "words#home"
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   resources :reviews
-  resources :words, only: [:index, :home]
+  resources :words, only: [:index, :home, :show]
   resources :users
 end
