8557b42f075023fa98a7f19fb74d62d32b4f1ee0
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-06 15:31:56 -0400

    Added test and function for admin viewing an index of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    if current_user.try(:admin?)
+      @users = User.all.order(created_at: :asc)
+    else
+      render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false)
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
