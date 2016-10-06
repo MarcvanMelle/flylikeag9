7b1eccd168f519b6c162c6c7832e5d428348c82b
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-06 14:59:58 -0400

    Added strong params to reviews controller, checked for validation on reviews#create and reviews#update with flash messages, wrote tests and functionality for admin updating reviews.

---
+++ spec/features/17_admin_updates_review_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature "Admin can update word" do
+  let!(:admin) { FactoryGirl.create(:admin) }
+  let!(:users) { FactoryGirl.create_list(:user, 2) }
+  let!(:word) { FactoryGirl.create(:word, user: users[0]) }
+  let!(:review) { FactoryGirl.create(:review, user: users[1], word: word) }
+
+  context "As an admin" do
+    scenario "I can update a review's body" do
+      login_as(admin, scope: :user)
+      visit word_path(word)
+
+      click_link "Edit Review"
+      fill_in("Review Text", with: "#{review.body} Updated at 10:30pm")
+      click_button "Save Review"
+      expect(page).to have_content "#{review.body} Updated at 10:30pm"
+    end
+
+    scenario "I cannot update a review's rating" do
+      login_as(admin, scope: :user)
+      visit word_path(word)
+
+      click_link "Edit Review"
+      expect(page).to have_no_content "Rating"
+    end
+  end
+
+  context "As non-admin authenticated user" do
+    scenario "I cannot curl into the update action" do
+      login_as(users[0], scope: :user)
+      page.driver.submit :patch, "/words/#{word.id}/reviews/#{review.id}", { review: { body: "I'm a hacker. I hacked your review, sucka" } }
+
+      expect(page).to have_content "You do not have permission to access this page!"
+    end
+  end
+end
