c9ac639508a7dfda84830a33aba9619f7b4a1431
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-06 16:22:48 -0400

    Added tests and function for admins to delete user accounts and all associated words and reviews.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,39 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    if authorized_party
+      if @user.valid?
+        @user.reviews.delete_all
+        @user.words.each do |word|
+          word.reviews.delete_all
+        end
+        @user.words.delete_all
+        @user.delete
+        flash[:success] = "User Account successfully closed"
+        admin_redirect
+      else
+        flash[:errors] = "User Account was not closed"
+        render user_path(@user)
+      end
+    else
+      render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false)
+    end
+  end
+
+  private
+
+  def authorized_party
+    current_user.try(:admin?) || current_user == @user
+  end
+
+  def admin_redirect
+    if current_user.admin
+      redirect_to users_path
+    else
+      redirect_to root_path
+    end
+  end
 end
