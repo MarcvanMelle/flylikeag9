dfe8f6d9fc30895f535e644fc6542738ad018046
Pete Eichman <peichman14@gmail.com>
2016-10-07 15:34:02 -0400

    Added tests and feature for adding new word

--- db/migrate/20161003184207_create_words.rb
+++ db/migrate/20161003184207_create_words.rb
@@ -1,9 +1,9 @@
 class CreateWords < ActiveRecord::Migration[5.0]
   def change
     create_table :words do |t|
-      t.belongs_to :user, foreign_key: true
-      t.string :word
-      t.string :definition
+      t.belongs_to :user, foreign_key: true, null: false
+      t.string :word, null: false
+      t.string :definition, null: false
 
       t.timestamps
     end
