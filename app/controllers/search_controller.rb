1555967929df7dfd98964da712a83468e656a454
“keyanestgit config --global user.email “keyan.est@gmail.com <YOUR@EMAIL.com>
2016-10-11 12:56:06 -0400

    Implemented and tested user search function

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,13 @@
+class SearchController < ApplicationController
+  def index
+    @word_results = Word.where("word LIKE (?)", "%#{params[:search]}%")
+    if @word_results.empty?
+      flash[:error] = "Word could not be found"
+      redirect_to words_path
+    end
+    if params[:search] == ""
+      flash[:error] = "You must enter something"
+      redirect_to words_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
       delete :remove_avatar
     end
   end
+  resources :search, only: [:index]
 end
