ac3becc9119cd1307060277f0449a4483871a5cd
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-12 12:48:22 -0400

    Added guest test

--- spec/features/24_user_votes_for_a_review_spec.rb
+++ spec/features/24_user_votes_for_a_review_spec.rb
@@ -24,8 +24,8 @@ feature "User can vote on a review" do
     end
 
     scenario "I can see an accurate vote-count associate with each review on the word" do
-      expect(page.find("#count#{review.id}").text).to eq("0")
-      expect(page.find("#count#{review2.id}").text).to eq("1")
+      expect(page.find("#count#{review.id}").text).to eq("Review Score: 0")
+      expect(page.find("#count#{review2.id}").text).to eq("Review Score: 1")
     end
   end
 
@@ -34,14 +34,14 @@ feature "User can vote on a review" do
       FactoryGirl.create(:vote, user: user1, review: review, up_down: "true")
       visit word_path(word)
       expect(page.find("#up#{review.id}")['class']).to eq("button upvoted")
-      expect(page.find("#count#{review.id}").text).to eq("1")
+      expect(page.find("#count#{review.id}").text).to eq("Review Score: 1")
     end
 
     scenario "I can click on the downvote button of a specific review, and the vote-count and vote buttons update" do
       FactoryGirl.create(:vote, user: user1, review: review, up_down: "false")
       visit word_path(word)
       expect(page.find("#down#{review.id}")['class']).to eq("button downvoted")
-      expect(page.find("#count#{review.id}").text).to eq("-1")
+      expect(page.find("#count#{review.id}").text).to eq("Review Score: -1")
     end
   end
 end
