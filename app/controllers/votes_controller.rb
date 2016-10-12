b6b47556ecf1246898a5a6576dda14eaea89274e
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-12 12:10:40 -0400

    Appease the Hound

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,26 +1,39 @@
 class VotesController < ApplicationController
 
   def create
-    reverse_vote = "false" if params[:vote][:up_down] == "true"
-    reverse_vote = "true" if params[:vote][:up_down] == "false"
-    check_same_votes = Vote.find_by(user: current_user, review_id: params[:vote][:review_id], up_down: params[:vote][:up_down])
-    check_oppose_votes = Vote.find_by(user: current_user, review_id: params[:vote][:review_id], up_down: reverse_vote)
-    if check_same_votes.nil? && check_oppose_votes.nil?
+    reverse_vote = reverse_the_vote
+    if check_same.nil? && check_oppose.nil?
       vote = Vote.new(vote_params)
       vote.save
-    elsif !check_same_votes.nil?
-      check_same_votes.destroy
-    elsif !check_oppose_votes.nil?
-      check_oppose_votes.update(up_down: params[:vote][:up_down])
+    elsif !check_same.nil?
+      check_same.destroy
+    elsif !check_oppose.nil?
+      check_oppose.update(up_down: params[:vote][:up_down])
     end
-    review = Review.find(params[:vote][:review_id])
-    vote_count = review.votes.where(review_id: review.id, up_down: "true").count - review.votes.where(review_id: review.id, up_down: "false").count
-    render json: vote_count.to_json
+    render json: count_the_vote.to_json
   end
 
   private
 
   def vote_params
     params.require(:vote).permit(:up_down, :review_id).merge(user: current_user)
+  end
+
+  def reverse_the_vote
+    return "false" if params[:vote][:up_down] == "true"
+    return "true" if params[:vote][:up_down] == "false"
+  end
+
+  def check_same
+    Vote.find_by(user: current_user, review_id: params[:vote][:review_id], up_down: params[:vote][:up_down])
+  end
+
+  def check_oppose
+    Vote.find_by(user: current_user, review_id: params[:vote][:review_id], up_down: reverse_the_vote)
+  end
+
+  def count_the_vote
+    review = Review.find(params[:vote][:review_id])
+    review.votes.where(review_id: review.id, up_down: "true").count - review.votes.where(review_id: review.id, up_down: "false").count
   end
 end
